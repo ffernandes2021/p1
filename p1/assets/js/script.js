430ad4a03e71bf5fd1007d3cb4aa2f89942bc3b5
Fabio <binhobinho1988@hotmail.com>
2022-04-23 18:08:26 -0300

    full-commit

--- p1/assets/js/script.js
+++ p1/assets/js/script.js
@@ -1,20 +1,13 @@
-function getPokemon(valor){
-    // Envia um HTTP Request, por meio de uma GET call (assíncrona)
-    para um endpoint definido
-    // Aguarda como dados vindos do response um JSON document
+function getPokemon(valor) {
     $.ajax({
-      url: "https://pokeapi.co/api/v2/pokemon/" + valor,
-      method: "get",
-      dataType: "json"
+        url: "https://pokeapi.co/api/v2/pokemon/" + valor,
+        method: "get",
+        dataType: "json"
     })
-    // Se o servidor retornar SUCCESS
-    .done(function(data){
-      // imprime os dados do response no console do browser
-      console.log(data);
+    .done(function (data) {
+        console.log(data);
     })
-    // Se o servidor retornar ERRO
-    .fail(function(jqXHR){
-      // imprime o HTTP error number
-      console.log("Erro HTTP: " + jqXHR.status);
-    });                     
-  } 
+    .fail(function (jqXHR) {
+        console.log("Erro HTTP: " + jqXHR.status);
+    });
+}
